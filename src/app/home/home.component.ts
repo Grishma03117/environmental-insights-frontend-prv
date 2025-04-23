ecaeb07e266d1ee1b1d8004a6788dca14cd264e0
Grishma03117 <2023mt03117@wilp.bits-pilani.ac.in>
2025-04-23 21:10:04 +0530

    fix: ops pipeline control

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
@@ -9,9 +10,47 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   navigateTo(route: string) {
     this.router.navigate([`/${route}`]);
+  }
+
+  trainModel() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const body = {};
+    this.http.post<any>(
+      'https://environmental-insights-main-app.azurewebsites.net/model-train',
+      body,
+      { headers }
+    ).subscribe({
+      next: (response: any) => {
+        console.log('Response recieved as', response);
+      },
+      error: () => {
+        console.log('Error');
+      },
+    });
+  }
+
+  dataPreprocess() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const body = {};
+    this.http.post<any>(
+      'https://environmental-insights-main-app.azurewebsites.net/data-preprocess',
+      body,
+      { headers }
+    ).subscribe({
+      next: (response: any) => {
+        console.log('Response recieved as', response);
+      },
+      error: () => {
+        console.log('Error');
+      },
+    });
   }
 }
